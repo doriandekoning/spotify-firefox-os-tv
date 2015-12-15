c5731ee1cea969bab480586407bac5834d9ce52e
unknown <doriandekoning@hotmail.com>
2015-12-15 20:05:53 +0100

    Fix login

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 // but without waiting for other external resources to load (css/images/etc)
 // That makes the app more responsive and perceived as faster.
 // https://developer.mozilla.org/Web/Reference/Events/DOMContentLoaded
+var clientid;
 window.addEventListener('DOMContentLoaded', function() {
 
   // We'll ask the browser to use strict code to help us catch errors earlier.
@@ -26,10 +27,38 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
 });
-
+var interval_poll_accescodes;
 $(document).ready(function(){
-  
-    $.ajax({url: "http://http://80.100.203.254:8888/auth", succes: function(result){
-        console.log(result);
-      }});
+  // $('#qr-code').svg({loadURL: 'http://80.100.203.254:8888/'});
+  /*  $.ajax({url: "http://80.100.203.254:8888/", dataType: "html", success: function(result){
+
+        $("#qr-code").html(result);
+        $("#qr-code").children().height(100);
+  /*      $.ajax({url: "http://80.100.203.254:8888/logedin", dataType: "html", success: function(result){
+          console.log(result);
+        }});*/
+    //}});
+  $.ajax({url: "http://80.100.203.254:8888/", dataType: "json", success: function(result){
+    clientid = result.client_id;
+    $("#qr-code").qrcode({
+      "size": 100,
+      "color": "#3a3",
+      "text": result.qr_url + result.client_id
+    });
+        console.log(clientid);
+    interval_poll_accescodes =
+    setInterval(function(result){
+      $.ajax({ 
+        url: "http://80.100.203.254:8888/poll_codes?id=" + clientid, 
+        success: function(data){
+          if(typeof data.access_token!= 'undefined'){
+            clearInterval(interval_poll_accescodes);
+          }
+        }, 
+        dataType: "json", 
+        timeout: 1000
+      });
+    }, 1000);
+  }});
+
 });
